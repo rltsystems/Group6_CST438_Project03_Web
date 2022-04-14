c9234096360c12110f5b308f206f55a3d0f85122
jbarrosoarr <jbarrosoarredondo@csumb.edu>
2022-04-13 18:12:52 -0700

    Added spring initializer -- basic files

---
+++ Group6_CST438_Project03_Web/src/main/java/Group6_CST438_Project03_Web/Group6Cst438Project03WebApplication.java
@@ -0,0 +1,13 @@
+package Group6_CST438_Project03_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group6Cst438Project03WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group6Cst438Project03WebApplication.class, args);
+	}
+
+}
---
+++ Group6_CST438_Project03_Web/src/test/java/Group6_CST438_Project03_Web/Group6Cst438Project03WebApplicationTests.java
@@ -0,0 +1,13 @@
+package Group6_CST438_Project03_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group6Cst438Project03WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
