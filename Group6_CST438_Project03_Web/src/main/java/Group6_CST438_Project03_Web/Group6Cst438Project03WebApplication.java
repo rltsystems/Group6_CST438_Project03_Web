b22b3f5ce01c46ddc835699826af2d9fd6e0c216
jbarrosoarr <jbarrosoarredondo@csumb.edu>
2022-04-13 18:26:09 -0700

    Added spring initializer

--- Group6_CST438_Project03_Web/src/main/java/Group6_CST438_Project03_Web/Group6Cst438Project03WebApplication.java
+++ Group6_CST438_Project03_Web/src/main/java/Group6_CST438_Project03_Web/Group6Cst438Project03WebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Group6Cst438Project03WebApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Group6Cst438Project03WebApplication.class, args);
 	}
 
